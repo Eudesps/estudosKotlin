9aa3619d0baa8c7b324a1dfe201bc67b17727764
eudes <jeudes894@gmail.com>
2024-05-13 15:46:54 -0300

    utilizando ViewBinding

--- app/src/main/java/aulaComponentesInterfaceViewBinding/ViewBindingActivity.kt
+++ app/src/main/java/aulaComponentesInterfaceViewBinding/ViewBindingActivity.kt
@@ -6,16 +6,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.componeteslistagemcolecoes.R
+import com.example.componeteslistagemcolecoes.databinding.ActivityViewBindingBinding
 
-class MainActivity : AppCompatActivity() {
+class ViewBindingActivity : AppCompatActivity() {
+    //private lateinit var binding: ActivityViewBindingBinding
+    private val binding by lazy { ActivityViewBindingBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        /*Aqui dentro é possível usar todos os componentes sem a necessidade de colocar o ""binding
+        antes de cada id*/
+        with(binding){
+            btnClique.setOnClickListener {  }
         }
     }
 }

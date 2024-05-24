d7a18da46c0bffc4c16185f6f2357254370586f8
eudes <jeudes894@gmail.com>
2024-05-24 14:42:27 -0300

    Button (FloatingActionButton)

---
+++ app/src/main/java/aulaComponentesInterfaceViewBinding/FloatingActionButtonActivity.kt
@@ -0,0 +1,23 @@
+package aulaComponentesInterfaceViewBinding
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.componeteslistagemcolecoes.R
+import com.example.componeteslistagemcolecoes.databinding.ActivityFloatingActionButtonBinding
+
+class FloatingActionButtonActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityFloatingActionButtonBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

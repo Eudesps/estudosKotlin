3cfbe011bd1082234e4e4ed40ab56645aafecbd9
eudes <jeudes894@gmail.com>
2024-06-10 08:59:35 -0300

    Swite e Toggle

--- app/src/main/java/aulaComponentesInterfaceViewBinding/FormularioActivity.kt
+++ app/src/main/java/aulaComponentesInterfaceViewBinding/FormularioActivity.kt
@@ -26,18 +26,47 @@ class FormularioActivity : AppCompatActivity() {
         with(binding){
             btnEnviar.setOnClickListener {
                 //checkbox()
+                //radioButton()
+                switchToggle()
             }
-            cbConfirmacao.setOnCheckedChangeListener { _, isChecked ->
+
+            /*cbConfirmacao.setOnCheckedChangeListener { _, isChecked ->
                 val resultado = if(isChecked) "SIM" else "NÃO"
                 binding.textResultado.text = "Valor selecionado $resultado"
-            }
+            }*/
+
             /*
             cbConfirmacao.setOnClickListener {
                 val selecionado = binding.cbConfirmacao.isChecked
                 val resultado = if(selecionado) "SIM" else "NÃO"
                 binding.textResultado.text = "Valor selecionado $resultado"
             }*/
+
+            /*rbMasculino.setOnClickListener {  }
+            rbMasculino.setOnCheckedChangeListener { buttonView, isChecked ->  }*/
         }
+    }
+
+    private fun switchToggle() {
+        val Switchmarcado = binding.switchNotificacoes.isChecked
+        val ToggleMarcado = binding.toggleAtivo.isChecked
+
+        val text = "Swite: $Switchmarcado , Toggle: $ToggleMarcado"
+
+        binding.textResultado.text = text
+    }
+
+    private fun radioButton(){
+        /*val selecionado = binding.rbMasculino.isChecked
+        binding.textResultado.text = if(selecionado) "Masculino" else "Feminino"*/
+
+        val idItemSelecionado = binding.rgSexo.checkedRadioButtonId
+        binding.textResultado.text = when(idItemSelecionado){
+            R.id.rbMasculino -> "Masculino"
+            R.id.rbFeminino -> "Feminino"
+            else -> "Nada selecionado"
+        }
+        binding.rgSexo.clearCheck()
     }
     private fun checkbox(){
         val selecionado = binding.cbConfirmacao.isChecked
